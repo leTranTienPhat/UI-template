a928a03a4da2b6241b78a6957383ede5d7d87eba
Dust07 <tienphat0707@gmail.com>
2023-05-25 17:10:27 +0700

    Refactor code

--- src/components/Showcase/CustomSlide/index.tsx
+++ src/components/Showcase/CustomSlide/index.tsx
@@ -10,7 +10,7 @@ function CustomSlide({ data }: Props) {
   const { name, company, desc, image, background } = data
   return (
     <div className="relative w-full h-screen text-white">
-      <img src={background} className="absolute w-full h-full" />
+      <img src={background} alt="background" className="absolute w-full h-full" />
       <div className="absolute border-4 border-white font-bold mx-10 my-20 text-[30px] p-4">
         {desc}
         <div className="absolute top-[-16px] left-[-16px] w-4 h-4  border-4 border-white"></div>
@@ -22,12 +22,12 @@ function CustomSlide({ data }: Props) {
       <div className="absolute bottom-8 left-16 z-20">
         <p className="font-bold text-3xl">{name}</p>
         <div className="flex gap-4 py-4">
-          <img src={logo} className="w-8 " />
+          <img src={logo} alt="logo" className="w-8" />
           <p className="font-bold text-xl">{company}</p>
         </div>
       </div>
       <div className="absolute w-full h-full z-10 ">
-        <img src={image} className="" />
+        <img src={image} alt="model image" className="" />
       </div>
       <div className="absolute bottom-36 right-16 z-30">
         <Ranking rating={5} />
